8f6107faffff57ec7ff56c11dacda1fdc87755a1
Rich B <richardbaker1993@gmail.com>
2024-03-28 12:49:03 +0000

    feat: bingo card formatted

--- bingo.js
+++ bingo.js
@@ -1,19 +1,30 @@
 "use strict";
 const { readFileSync } = require("fs");
 
-
+// Serve our data from our txt file.
 const data = readFileSync("Part1.txt", "utf-8");
 // console.log(data);
 
+let noSpace = data.split("\n");
+noSpace.splice(1, 1);
 
-let noSpace = data.split('\n');
-noSpace.splice(1,1);
-console.log(noSpace);
+const calledNumbers = noSpace
+  .shift()
+  .split(",")
+  .map((n) => Number(n));
+// console.log(calledNumbers);
 
+// Extract the remaining indices and use them in a 2d Matrix.
+const card = [];
+for (let i of noSpace) {
+  card.push(i.split(" ").map((n) => Number(n)));
+}
 
-const calledNumbers = noSpace[0].split(',').map(n => Number(n));
 console.log(calledNumbers);
+console.log(card);
 
-
+// Now we can check if specific numbers are included on the card from the called number
+// list:
+// console.log(calledNumbers.includes(card[0][0]));
 
 module.exports = { noSpace, calledNumbers };
